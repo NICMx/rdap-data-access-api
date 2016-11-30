0bf7f173dfe89d794a35c3b269b92347b2f20e93
dalpuche <alpuche1990@gmail.com>
2016-11-30 11:47:34 -0600

    Add ipnetwork and autnum delete statements

--- src/main/resources/META-INF/sql/Delete.sql
+++ src/main/resources/META-INF/sql/Delete.sql
@@ -1,128 +1,161 @@
-
-#02_delete_entity_contact
+#01_delete_entity_contact
 DELETE  FROM rdap.entity_contact;
 
-#03_delete_entity_entity_roles 
+#02_delete_entity_entity_roles 
 DELETE  FROM rdap.entity_entity_roles;
 
-#04_delete_entity_links
+#03_delete_entity_links
 DELETE  FROM rdap.entity_links;
 
-#05_delete_entity_public_ids
-DELETE  FROM rdap.entity_public_ids;
-
-#06_delete_entity_remarks
+#04_delete_entity_remarks
 DELETE  FROM rdap.entity_remarks;
 
-#07_delete_entity_status
+#05_delete_entity_status
 DELETE  FROM rdap.entity_status;
 
-#08_delete_domain_entity_roles 
-DELETE  FROM rdap.domain_entity_roles;
-
-#09_delete_nameserver_entity_roles 
-DELETE  FROM rdap.nameserver_entity_roles;
-
-#10_delete_entity_events
+#06_delete_entity_events
 DELETE  FROM rdap.entity_events;
 
-
-#11_delete_entity
-DELETE  FROM rdap.entity;
-
-
-
-#12_delete_vcard_postal_info
+#07_delete_vcard_postal_info
 DELETE  FROM rdap.vcard_postal_info;
 
-#13_delete_vcard
+#08_delete_vcard
 DELETE  FROM rdap.vcard;
+
+#09_delete_entity_public_ids
+DELETE  FROM rdap.entity_public_ids;
+
+#10_delete_nameserver_entity_roles 
+DELETE  FROM rdap.nameserver_entity_roles;
+
+#11_delete_nameserver_links
+DELETE  FROM rdap.nameserver_links;
+
+#12_delete_nameserver_remarks
+DELETE  FROM rdap.nameserver_remarks;
+
+#13_delete_nameserver_status
+DELETE  FROM rdap.nameserver_status;
 
 #14_delete_nameserver_events
 DELETE  FROM rdap.nameserver_events;
 
-#15_delete_nameserver_links
-DELETE  FROM rdap.nameserver_links;
-
-#16_delete_nameserver_remarks
-DELETE  FROM rdap.nameserver_remarks;
-
-#17_delete_nameserver_status
-DELETE  FROM rdap.nameserver_status;
-
-#18_delete_ip_address
+#15_delete_ip_address
 DELETE  FROM rdap.ip_address;
 
-#19_delete_domain_nameservers
+#16_delete_domain_nameservers
 DELETE  FROM rdap.domain_nameservers;
 
-#20_delete_nameserver
-DELETE  FROM rdap.nameserver;
+#17_delete_domain_entity_roles 
+DELETE  FROM rdap.domain_entity_roles;
 
-#21_delete_domain_status
+#18_delete_domain_links
+DELETE  FROM rdap.domain_links;
+
+#19_delete_domain_remarks
+DELETE  FROM rdap.domain_remarks;
+
+#20_delete_domain_status
 DELETE  FROM rdap.domain_status;
 
-#22_delete_domain_remarks
-DELETE  FROM rdap.domain_remarks;
+#21_delete_domain_events
+DELETE  FROM rdap.domain_events;
+
+#22_delete_domain_networks
+DELETE  FROM rdap.domain_networks;
 
 #23_delete_domain_public_ids
 DELETE  FROM rdap.domain_public_ids;
 
-#24_delete_domain_events
-DELETE  FROM rdap.domain_events;
-
-#25_delete_domain_networks
-DELETE  FROM rdap.domain_networks;
-
-#26_delete_domain_links
-DELETE  FROM rdap.domain_links;
-
-#27_delete_ds_links
+#24_delete_ds_links
 DELETE  FROM rdap.ds_links;
 
-#28_delete_ds_events
+#25_delete_ds_events
 DELETE  FROM rdap.ds_events;
 
-#29_delete_ds_data
+#26_delete_ds_data
 DELETE  FROM rdap.ds_data;
 
-#30_delete_variant_relation
+#27_delete_variant_relation
 DELETE  FROM rdap.variant_relation;
 
-#31_delete_variant_name
+#28_delete_variant_name
 DELETE  FROM rdap.variant_name;
 
-#32_delete_variant
+#29_delete_variant
 DELETE  FROM rdap.variant;
 
-#33_delete_secure_dns
+#30_delete_secure_dns
 DELETE  FROM rdap.secure_dns;
 
-#34_delete_domain
-DELETE  FROM rdap.domain;
-
-#35_delete_zone
+#31_delete_zone
 DELETE  FROM rdap.zone;
 
+#32_delete_asn_entity_roles
+DELETE  FROM rdap.asn_entity_roles;
 
+#33_delete_asn_links
+DELETE  FROM rdap.asn_links;
 
-#38_delete_event_links
+#34_delete_asn_remarks
+DELETE  FROM rdap.asn_remarks;
+
+#35_delete_asn_status
+DELETE  FROM rdap.asn_status;
+
+#36_delete_asn_events
+DELETE  FROM rdap.asn_events;
+
+#37_delete_ip_network_parent_relation
+DELETE  FROM rdap.ip_network_parent_relation;
+
+#38_delete_ipn_entity_roles
+DELETE  FROM rdap.ipn_entity_roles;
+
+#39_delete_ip_network_links
+DELETE  FROM rdap.ip_network_links;
+
+#40_delete_ip_network_remarks
+DELETE  FROM rdap.ip_network_remarks;
+
+#41_delete_ip_network_status
+DELETE  FROM rdap.ip_network_status;
+
+#42_delete_ip_network_events
+DELETE  FROM rdap.ip_network_events;
+
+#43_delete_event_links
 DELETE  FROM rdap.event_links;
 
-#39_delete_remark_links
+#44_delete_remark_links
 DELETE  FROM rdap.remark_links;
 
-#40_delete_event
-DELETE  FROM rdap.event;
-
-#41_delete_link
+#45_delete_link
 DELETE  FROM rdap.link;
 
-#42_delete_public_id
+#46_delete_event
+DELETE  FROM rdap.event;
+
+#47_delete_public_id
 DELETE  FROM rdap.public_id;
 
-#43_delete_remark_description
+#48_delete_remark_description
 DELETE  FROM rdap.remark_description;
 
-#44_delete_remark
+#49_delete_remark
 DELETE  FROM rdap.remark;
+
+#50_delete_entity
+DELETE  FROM rdap.entity;
+
+#51_delete_nameserver
+DELETE  FROM rdap.nameserver;
+
+#52_delete_domain
+DELETE  FROM rdap.domain;
+
+#53_delete_ip_networks
+DELETE  FROM rdap.ip_network;
+
+#54_delete_asn
+DELETE  FROM rdap.autonomous_system_number;
