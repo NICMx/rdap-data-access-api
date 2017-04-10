c733cf1a0d5592d21a9af92e34226f6e4e9a9fc8
Alberto Leiva Popper <ydahhrk@gmail.com>
2017-04-10 11:34:45 -0500

    Add missing API documentation.
    It's actually so small it's pretty much pointless.
    There's not much to say about it because it's not used much.

--- src/main/java/mx/nic/rdap/db/spi/RdapUserDAO.java
+++ src/main/java/mx/nic/rdap/db/spi/RdapUserDAO.java
@@ -3,8 +3,14 @@ package mx.nic.rdap.db.spi;
 import mx.nic.rdap.db.RdapUser;
 import mx.nic.rdap.db.exception.RdapDataAccessException;
 
+/**
+ * Interface for {@link RdapUser} DAO functions.
+ */
 public interface RdapUserDAO extends DAO {
 
+	/**
+	 * Returns the user whose username is <code>username</code>.
+	 */
 	public RdapUser getByUsername(String username) throws RdapDataAccessException;
 
 }
