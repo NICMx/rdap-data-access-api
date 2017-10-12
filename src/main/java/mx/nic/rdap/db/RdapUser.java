11720b75e3a0e3930a982be0b54f22c34585fb90
Paco Moreno <fjarana@itesm.mx>
2017-10-12 11:45:59 -0500

    Remove property "id" from RdapUser

--- src/main/java/mx/nic/rdap/db/RdapUser.java
+++ src/main/java/mx/nic/rdap/db/RdapUser.java
@@ -10,7 +10,6 @@ import java.util.Set;
  */
 public class RdapUser {
 
-	private Long id;
 	private String name;
 	private String pass;
 	private Integer maxSearchResults;
@@ -29,7 +28,6 @@ public class RdapUser {
 		int result = 1;
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		result = prime * result + ((pass == null) ? 0 : pass.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((maxSearchResults == null) ? 0 : maxSearchResults.hashCode());
 		return result;
 	}
@@ -53,32 +51,12 @@ public class RdapUser {
 				return false;
 		} else if (!pass.equals(other.pass))
 			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
 		if (maxSearchResults == null) {
 			if (other.maxSearchResults != null)
 				return false;
 		} else if (!maxSearchResults.equals(other.maxSearchResults))
 			return false;
 		return true;
-	}
-
-	/**
-	 * @return the id
-	 */
-	public Long getId() {
-		return id;
-	}
-
-	/**
-	 * @param id
-	 *            the id to set
-	 */
-	public void setId(Long id) {
-		this.id = id;
 	}
 
 	/**
